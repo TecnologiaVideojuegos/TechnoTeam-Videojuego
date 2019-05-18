d234b6b67ba651b6bf495f50fd7d2f894ed0a3bc
AndresGS <jandres.gomez@edu.uah.es>
2019-05-18 13:22:01 +0200

    commit

--- src/utils/EnemyData.java
+++ src/utils/EnemyData.java
@@ -15,6 +15,8 @@ public class EnemyData {
                 //400 Orc
                 put(401, new Enemy("Orc",true, 401, new SpriteGeneral(401, 0), 1184, 224, "graphic/enemySprite/avatar401.png"));
                 //400 Orc
+                put(402, new Enemy("Orc",true, 402, new SpriteGeneral(402, 1), 528, 576, "graphic/enemySprite/avatar404.png"));
+                //400 Orc
                 put(404, new Enemy("Orc",true, 404, new SpriteGeneral(404, 2), 528, 576, "graphic/enemySprite/avatar404.png"));
                 //400 Orc
                 put(403, new Enemy("Orc",true, 403, new SpriteGeneral(403, 0), 640, 384, "graphic/enemySprite/avatar403.png"));
